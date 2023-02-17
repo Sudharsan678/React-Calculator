e85206bb044e4be9a24738d12bf3365dcdb23b12
Sudharsan S <sudharsanvsr@gmail.com>
2023-02-17 11:43:53 +0530

    Fully Functional React calculator

---
+++ src/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Todo({todo}) {
+  return (
+    <div>
+        <label>
+            <input type = 'checkbox' checked = {todo.complete} />
+        </label>
+    </div>
+  )
+}
